5a65e2edf78c73f655499220e7a5d7f719d4a327
asherpereira <pereiraasher0@gmail.com>
2026-09-25 15:27:47 +1000

    Add src/Nova.Client.Windows/MainWindow.xaml.cs

---
+++ src/Nova.Client.Windows/MainWindow.xaml.cs
@@ -0,0 +1,3 @@
+using Microsoft.UI.Xaml;
+namespace Nova.Client.Windows;
+public sealed partial class MainWindow : Window { public MainWindow(){InitializeComponent();} private void SignIn_Click(object sender,RoutedEventArgs e) { } }
