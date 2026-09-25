335239017bb7399b68f910813e028a71159a77d3
asherpereira <pereiraasher0@gmail.com>
2026-09-25 15:27:43 +1000

    Add src/Nova.Client.Windows/App.xaml.cs

---
+++ src/Nova.Client.Windows/App.xaml.cs
@@ -0,0 +1,3 @@
+using Microsoft.UI.Xaml;
+namespace Nova.Client.Windows;
+public partial class App : Application { public static Window? MainWindow {get;private set;} public App(){InitializeComponent();} protected override void OnLaunched(LaunchActivatedEventArgs args){MainWindow=new MainWindow();MainWindow.Activate();} }
